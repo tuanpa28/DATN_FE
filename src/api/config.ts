93682bd23d04273a3244b911eca88d278fba4f07
tuanpa03 <phamanhtuan2833@gmail.com>
2023-12-29 01:50:09 +0700

    update

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const instance = axios.create({
-  baseURL: 'http://localhost:8080/api/',
+  baseURL: 'https://datn-be-zqom.onrender.com/api/',
 });
 
 instance.interceptors.request.use(
